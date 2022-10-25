976c1ff36de19743dd08bc8bb03141e9217486d9
OksanaTarun <oxana.tarun@gmail.com>
2022-10-26 00:10:23 +0300

    slice commit

---
+++ task_score.js
@@ -0,0 +1,9 @@
+let introduction = 100;
+let git = 319;
+let js = 120;
+let avgAll = (introduction + git + js) / 3;
+let myName = "Oksana";
+let surname = "Tarun";
+console.log(
+  `${myName.slice(0, 1)}.${surname}. Average score for all modules: ${avgAll}.`
+);
